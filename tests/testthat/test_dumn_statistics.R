01c230e66c15b88e2a59aeb1be6d0523b075ca68
Kevin Wang <kevinwang@smu.edu>
2021-10-28 16:43:12 -0500

    New changes.

--- tests/testthat/test_dumn_statistics.R
+++ tests/testthat/test_dumn_statistics.R
@@ -6,7 +6,7 @@ test_that("Test Add", {
   expect_equal(`%add%`(-1, 0.1), -0.9)
   expect_equal(`%add%`(-0.3, -0.001), -0.301)
   expect_equal(`%add%`(3, 1), 4)
-  expect_equal(`%add%`(2, 5), 7)
+  expect_equal(`%add%`(2, 3), 5)
 })
 
 
