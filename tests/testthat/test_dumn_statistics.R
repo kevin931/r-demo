cbc71baf1d279ba4294dac412a83da947c4b5aef
Kevin Wang <kevinwang@smu.edu>
2021-10-28 16:30:55 -0500

    Corrected test.

--- tests/testthat/test_dumn_statistics.R
+++ tests/testthat/test_dumn_statistics.R
@@ -4,5 +4,5 @@ library(testthat)
 test_that("Test Add", {
   expect_equal(`%add%`(1, 1), 2)
   expect_equal(`%add%`(-1, 0.1), -0.9)
-  expect_equal(`%add%`(-0.3, -0.001), -0.302)
+  expect_equal(`%add%`(-0.3, -0.001), -0.301)
 })
