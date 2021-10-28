c51f493585cb0ee8999c83df636af6d11d2eac8e
Yuqiu Yang <yyqengels@gmail.com>
2021-10-28 16:34:46 -0500

    sdfjasl

--- tests/testthat/test_dumn_statistics.R
+++ tests/testthat/test_dumn_statistics.R
@@ -5,4 +5,7 @@ test_that("Test Add", {
   expect_equal(`%add%`(1, 1), 2)
   expect_equal(`%add%`(-1, 0.1), -0.9)
   expect_equal(`%add%`(-0.3, -0.001), -0.301)
+  expect_equal(`%add%`(3, 1), 4)
 })
+
+
